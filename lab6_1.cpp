16dc8762ea0660f60c7d649f5aace2cbaafabb25
Siwagon2546 <netevgjqw@gmail.com>
2022-12-10 17:03:23 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int x;
+    int sum_Odd = 0;
+    int sum_Even = 0;
+    
+    while(true){
+        cout << "Enter an integer: ";
+        cin>>x;
+        if(x != 0 ){
+            if(x%2 == 1){
+                sum_Odd +=1;
+            }
+           else{    
+                sum_Even +=1;
+            }
+        }
+        else{
+            cout << "#Even numbers = "<< sum_Even <<"\n";
+            cout << "#Odd numbers = "<< sum_Odd <<"\n";
+            return false;
+        }
+    }
+    
+
+    
     return 0;
 }
